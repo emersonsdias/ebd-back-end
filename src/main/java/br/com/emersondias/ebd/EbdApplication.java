5c2cf3e61939959cc3ad8099fbd959bd02ebaeef
Emerson Dias <emersondias@hotmail.com>
2025-02-02 19:12:30 -0300

    Create SpringBoot project

---
+++ src/main/java/br/com/emersondias/ebd/EbdApplication.java
@@ -0,0 +1,13 @@
+package br.com.emersondias.ebd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emersondias/ebd/EbdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emersondias.ebd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
