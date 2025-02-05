38aec7c63843473ee3fdd56055aa743aab3bab1e
Emerson Dias <emersondias@hotmail.com>
2025-02-05 08:46:22 -0300

    Add validation exception handler

--- src/main/java/br/com/emersondias/ebd/exceptions/handler/ControllerExceptionHandler.java
+++ src/main/java/br/com/emersondias/ebd/exceptions/handler/ControllerExceptionHandler.java
@@ -6,12 +6,15 @@ import br.com.emersondias.ebd.utils.LogHelper;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @ControllerAdvice
 @ApiResponses(value = {
@@ -34,6 +37,25 @@ public class ControllerExceptionHandler {
                 .message("Não foi encontrado o recurso solicitado [" + e.getClassType().getSimpleName() + ":" + e.getTerm() + "]")
                 .path(request.getRequestURI())
                 .build();
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardErrorDTO> validationError(MethodArgumentNotValidException e,
+                                                            HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        var error = StandardErrorDTO.builder()
+                .timestamp(Instant.now())
+                .status(status.value())
+                .error("Entidade não processável")
+                .message("Erro de validação de dados")
+                .path(request.getRequestURI())
+                .build();
+        e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .filter(Objects::nonNull)
+                .forEach(error::addAdditionalData);
         return ResponseEntity.status(status).body(error);
     }
 
